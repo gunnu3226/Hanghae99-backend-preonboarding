402cb57faa25043353cae73fe02b68c918f730fa
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-07-10 15:59:03 +0900

    feat/token : add create accessToken method

--- src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
+++ src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
@@ -1,5 +1,11 @@
 package com.hanghae99.preonboardingbackend.config.jwt;
 
+import com.hanghae99.preonboardingbackend.model.entity.Authority;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +16,10 @@ import java.security.Key;
 @Configuration
 @Slf4j
 public class TokenProvider implements InitializingBean {
+    private static final String BEARER_PREFIX = "Bearer ";
+    public static final String USER_ID = "userId";
     private static final String AUTHORITIES_KEY = "authorities";
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
     @Value("${jwt.secret}")
     private String secret;
     @Value("${jwt.token-validity-in-milliseconds}")
@@ -19,6 +28,24 @@ public class TokenProvider implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
+    public String createAccessToken(
+        final Long userId,
+        final String username,
+        final Set<Authority> authorities
+    ) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+            Jwts.builder()
+                .setSubject(username)
+                .claim(USER_ID, userId)
+                .claim(AUTHORITIES_KEY, authorities)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + tokenValidityInMilliseconds))
+                .signWith(key, signatureAlgorithm)
+                .compact();
     }
 }
